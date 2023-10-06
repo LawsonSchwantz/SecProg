2d1cfc18fda01d5da0d7929154e870e25f342a21
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-06 13:03:07 +0700

    Delete config/connection.php

--- config/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once(__DIR__ . '/../config/database.php');
-
-    $connection = new mysqli(
-        $config['server'],
-        $config['username'],
-        $config['password'],
-        $config['database']
-    );
