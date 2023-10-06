b0374364c2eca2a04b7582eb695a68551cb12522
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-06 12:57:14 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,1 +1,7 @@
-
+<?php
+    $config = [
+        'server' => '127.0.0.1',
+        'username' => 'admin',
+        'password' => 'bebekkalojalanada23112',
+        'database' => 'bebekgoreng',
+    ];
