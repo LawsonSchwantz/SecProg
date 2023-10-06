379ca2f8c2e9f54efbcd768e2a88df5e44d53964
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-06 17:10:37 +0700

    Update report.php

--- report.php
+++ report.php
@@ -13,13 +13,13 @@
 <form action="controllers/ReportController.php" method="POST">
     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token']; ?>" />
     <label for="firstname" class="form-label text-light">First Name</label>
-    <input id="firstname" name="name" type="text" placeholder="" class="form-control" required>
+    <input id="firstname" name="firstname" type="text" placeholder="" class="form-control" required>
     <label for="lastname" class="form-label text-light">Last Name</label>
-    <input id="lastname" name="name" type="text" placeholder="" class="form-control" required>
+    <input id="lastname" name="lastname" type="text" placeholder="" class="form-control" required>
     <label for="email" class="form-label text-light">Your Email Address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="email" required/>
-    <label for="pesan" class="form-label text-light">Feedback</label>
-    <textarea class="form-control" id=pesan required></textarea>
+    <input type="email" class="form-control" id="email" name="email" aria-describedby="email" required/>
+    <label for="feedback" class="form-label text-light">Feedback:</label>
+    <textarea class="form-control" id="feedback" name="feedback"required></textarea>
     <button type="submit" class="btn btn-warning">Send data</button>
 </form>
 
