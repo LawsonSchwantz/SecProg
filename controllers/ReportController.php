9c2de3b0bb00b4b57e6c97acd9b35d85898c957b
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-06 18:37:12 +0700

    Add files via upload

---
+++ controllers/ReportController.php
@@ -0,0 +1,41 @@
+<?php
+
+    session_start();
+    require_once(__DIR__ . '/connection.php');
+    $report_typelist = array(
+        "1" => "Kritik dan Saran",
+        "2" => "Pengajuan Keluhan",
+        "3" => "Lainnya"
+    );
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if(isset($_POST['send_data'])){
+            $report_type = $_POST['report_type'];
+            $description = $_POST['description'];
+
+            $description = htmlentities($description);
+
+            $error = false;
+
+            if(!in_array($report_type,array_keys($report_typelist))){
+                $_SESSION['error_report'] = "Please enter a valid report type!";
+                $error = true;
+            }else if(strlen($description) >= 1000){
+                $_SESSION['error_report'] = "Too Long, description length must be below 1000 characters!";
+                $error = true;
+            }
+            if($error){
+                header("Location: ../report.php");
+                die;
+            }
+
+            $user_id = $_SESSION['user_id'];
+            $query = "INSERT INTO report VALUES (NULL, $user_id, '$report_type', '$feedback', NOW());";
+
+            $connection->query($query);
+
+            $_SESSION['report_success'] = "Report has been successfully sent!";
+
+            header("Location: ../report.php");
+        }
+    }
+    
