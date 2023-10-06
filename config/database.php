7a5849f0f9e4f23d0e3e91e81812cb685a04e959
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-06 16:14:37 +0700

    Update and rename db.php to database.php

--- config/database.php
+++ config/database.php
@@ -3,5 +3,5 @@
         'server' => '127.0.0.1',
         'username' => 'admin',
         'password' => 'bebekkalojalanada23112',
-        'database' => 'bebekgoreng',
+        'database' => 'SecureProg',
     ];
