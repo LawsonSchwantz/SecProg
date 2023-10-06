7ed3e09bca16b98d877f7d9966a0ed05f5b8b0f8
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-06 17:22:46 +0700

    Update report.php

--- report.php
+++ report.php
@@ -12,7 +12,7 @@
 
 <form action="controllers/ReportController.php" method="POST">
     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token']; ?>" />
-    <label for="feedback" class="form-label text-light">Feedback:</label>
+    <label for="feedback" class="form-label text-light">Tuliskan keluhanmu dibawah ini:</label>
     <textarea class="form-control" id="feedback" name="feedback"required></textarea>
     <button type="submit" class="btn btn-warning">Send data</button>
 </form>
