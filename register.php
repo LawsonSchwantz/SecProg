a46eb24661a994745156738e81496df384790683
fefe <88881191+fefethecyberclown@users.noreply.github.com>
2023-10-07 15:02:56 +0700

    Create register.php

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h1>Register</h1>
+    <form action="controller/registerc.php" method="POST">
+    <div class="form-group">
+        <label for="">Email</label>
+        <input type="email" class="form-control" name="email">
+    </div>
+    <div class="form-group">
+        <label for="">Username</label>
+        <input type="text" class="form-control" name="username">
+    </div>
+    <div class="form-group">
+        <label for="">Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <div class="form-group">
+        <label for="">Confirm Password</label>
+        <input type="confirmpass" class="form-control" name="confirmpassword">
+    </div>
+    <button name="register">Register</button>
+    <br>
+    <a href="login.php" class="pull-right">Already Have Account? Login Now</a>
+    </form>
+</body>
