3de4a361c58e9894f129fe5ef0af8bb9cea5b961
fefe <88881191+fefethecyberclown@users.noreply.github.com>
2023-10-07 15:03:28 +0700

    Create loginc.php

---
+++ controllers/loginc.php
@@ -0,0 +1,39 @@
+<?php
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $validate = 1;
+
+    if(strlen($email)==0){
+        $validate=0;
+        $error='Email cannot be empty!';
+    }else if(strlen($password)==0){
+        $validate=0;
+        $error='Password Cannot be Empty!';
+    }
+    $con = mysqli_connect('localhost', 'root', '', 'secprog', 3306);
+    if ($con->error){
+        echo $con->error;
+    }
+    else{
+        if($validate==1){
+            $result = $con->query("SELECT * FROM users WHERE `email`='$email' AND `password`='$password'");
+            if($result->num_rows == 1){
+                $dataresult = $result->fetch_assoc();
+                session_start();
+                $_SESSION["loggedin"] = true;
+                $_SESSION["username"] = $dataresult["username"];
+                $_SESSION["email"] = $dataresult["email"];
+                $_SESSION["id"] = $dataresult["id"];
+                header("Location: ../index.php");
+                die();
+            }else{
+                $error ='Wrong Username and Pasword!';
+                header("Location: ../login.php?error=".$error);
+            }
+        }else{
+            header("Location: ../login.php?error=".$error);
+        }
+    }
+
+?>
