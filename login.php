6ee05efc29d5e86448e8233f70c1dffbd2b8d633
fefe <88881191+fefethecyberclown@users.noreply.github.com>
2023-10-07 15:02:34 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="controller/loginc.php" method="POST">
+    <div class="form-group">
+        <label for="">Username</label>
+        <input type="text" class="form-control" name="username">
+    </div>
+    <div class="form-group">
+        <label for="">Password</label>
+        <input type="password" class="form-control" name="password">
+    </div>
+    <button class="btn btn-primary" name="login">Login</button>
+    <br>
+    <a href="register.php" class="pull-right">Don't Have Account? Register First</a>
+    </form>
