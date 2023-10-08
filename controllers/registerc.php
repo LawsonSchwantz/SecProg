ddb8c567a0f075689813861bc8cf4c5f817edb39
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-08 16:44:22 +0700

    Update registerc.php

--- controllers/registerc.php
+++ controllers/registerc.php
@@ -1,38 +1,63 @@
 <?php
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $validate = 1;
 
-    $con = mysqli_connect('localhost', 'root', '', 'SecureProg', 3306);
-    if(strlen($username)==0){
-        $validate=0;
-        $error='Username cannot be empty!';
-    }else if(strlen($username)<5 || strlen($username)>15){
-        $validate=0;
-        $error='Username length must be 5-15 characters!';
-    }else if(strlen($password)==0){
-        $validate=0;
-        $error='Password cannot be empty!';
-    }else if(strlen($password)<8){
-        $validate=0;
-        $error='Password length must be atleast 8 characters!';
-    }if ($con->error){
-        echo $con->error;
-    }else {
-        if($validate==1){
-            $result = $con->query("SELECT * FROM users WHERE `username`='$username'");
-            if($result->num_rows == 1){
-                $error ='Username has been taken!';
-                $_POST["register"]=true;
-                header("Location: ../register.php?error=".$error.'?reg=1');
+    session_start();
+    require_once(__DIR__ . '/connection.php');
+
+    function generateCSRFToken() {
+        if (!isset($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
+    }
+
+    function validateCSRFToken($token) {
+        return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['csrf_token']) && validateCSRFToken($_POST['csrf_token'])) {
+        $username = htmlspecialchars($_POST['username']);
+        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $password = $_POST["password"];
+        $confirm_pass = $_POST["confirmpassword"];
+        $validate = 1;
+        
+        if(strlen($username)==0){
+            $validate=0;
+            $error='Username cannot be empty!';
+        }else if(strlen($username)<5 || strlen($username)>15){
+            $validate=0;
+            $error='Username length must be 5-15 characters!';
+        }else if(strlen($password)==0){
+            $validate=0;
+            $error='Password cannot be empty!';
+        }else if(strlen($password)<8){
+            $validate=0;
+            $error='Password length must be atleast 8 characters!';
+        }
+        
+        if ($connection->error){
+            echo $connection->error;
+        }
+        else {
+            if($validate==1){
+                $result = $connection->query("SELECT * FROM users WHERE `username`='$username'");
+                if($result->num_rows == 1){
+                    $error ='Username has been taken!';
+                    header("Location: ../register.php?error=".$error.'?reg=1');
+                }else if($confirm_pass != $password){
+                    $error ='Password did not match!';
+                    header("Location: ../register.php?error=".$error.'?reg=1');
+                }
+                else{
+                    $password = password_hash($password,PASSWORD_BCRYPT);
+                    $connection->query("INSERT INTO `users` (`id`,`username`, `email`, `password`) VALUES (NULL,'$username', '$email', '$password', NOW());");
+                    $_POST["register"] = "Register Successfull";
+                    header("Location: ../index.php");
+                }
             }else{
-                $con->query("INSERT INTO `users` (`id`,`username`, `email`, `password`) VALUES (NULL,'$username', '$email', '$password');");
-                header("Location: ../index.php");
+                header("Location: ../register.php?error=".$error.'&reg=1');
             }
-        }else{
-            $_POST["register"]=true;
-            header("Location: ../register.php?error=".$error.'&reg=1');
         }
     }
+    generateCSRFToken();
 ?>
