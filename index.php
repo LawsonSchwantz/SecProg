0b6ad895fd801e8a9994bca0b6fc00749f135915
pweesang <125939718+pweesang@users.noreply.github.com>
2023-10-18 01:03:24 +0700

    index
    not done yet

---
+++ index.php
@@ -0,0 +1,218 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Header with Navigation Menu</title>
+    <style>
+        header {
+            background-color: #333;
+            display: block;
+            color: #fff;
+            padding: 1%;
+            position:sticky;
+            
+            top:0px;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            display: inline;
+            margin-right: 3%;
+        }
+
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+        #login{
+            display: inline;
+            margin-right : 3%;
+        }
+        /* footer{
+            position:sticky;
+            bottom:0px;
+            background-color: white;
+            color: #fff;
+            padding: 0%;
+        } */
+        .footer{
+            color:black;
+            margin-left:3%;
+            margin-right:3%;
+            margin-top:90%%
+        }
+        hr{
+            color:lightgrey;
+            
+        }
+        .custom-table {
+         width: 100%;
+         border-collapse: collapse;
+         
+        }
+        .custom-table td {
+        border: 1px solid #ccc;
+        padding: 10px;
+        text-align: center;
+        margin-right : 3%;
+         margin-left: 3%;
+        }
+        /* .custom-table th {
+        background-color: #333;
+        color: #fff;
+        } */
+        .custom-table tbody tr:nth-child(4){
+        background-color: #fff;
+        margin-right : 3%;
+        margin-left: 3%;
+        }
+     
+
+    </style>
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about-us.php">About</a></li>
+           <!-- <li><a href="#">Services</a></li>-->
+            <li><a href="report.php">Report</a></li>
+            
+
+        </ul>
+    </header>
+
+    <main>
+        <p>This is the main content of the page.</p>
+        <table class="custom-table">
+        <!-- PLEASE READ NOTES ON THE BOTTOM OF THIS CODE -->
+        <tbody>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+            </tr>
+            <!-- Add more rows as needed -->
+        </tbody>
+    </table>
+    </main>
+
+    <footer>
+        <hr></hr>
+        <p class="footer">&copy; 2023 Underdev</p>
+    </footer>
+</body>
+</html>
+
+<?php
+
+//    if ($_SESSION['is_login'] !== true) {
+        echo "<header><ul><div id='login'><a href='login.php'>Logout</a></div></ul></header>";
+//    }else{
+//        echo "<div id='login'><a href='login.php'>Login</a></div>";
+//    }
+?>
+
+<!-- 
+    - Table masih berupa dummy content. 
+    - next progress adalah bikin php untuk auto generate jumlah table sesuai dengan jumlah barang yang dimiliki dalam database
+    -->
