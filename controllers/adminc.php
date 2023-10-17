3352581291717b49f69f73da4691a2bc74a497ff
fefe <88881191+fefethecyberclown@users.noreply.github.com>
2023-10-17 16:15:19 +0700

    Add adminc.php backend blom jalan

--- controllers/adminc.php
+++ controllers/adminc.php
@@ -3,18 +3,18 @@ session_start();
 require_once(__DIR__ . '/connection.php');
 
 if ($_SESSION['is_login'] !== true) {
-    header("Location: login.php"); // Redirect non-admin users
+    header("Location: login.php"); 
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if (isset($_POST['edit_report'])) {
-        // Handle edit report action
+
         $report_id = $_POST['report_id'];
-        // Perform the necessary update actions here
+      
     } elseif (isset($_POST['delete_report'])) {
-        // Handle delete report action
+        
         $report_id = $_POST['report_id'];
-        // Perform the necessary delete actions here
+      
     }
 }
 
