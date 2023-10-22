05ba120f893a38c2964c20e0db9cb8333e868457
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-10-22 23:28:24 +0700

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
-    require_once(__DIR__ . '/controllers/connection.php');
     session_start();
-    if ($_SESSION['is_login'] !== true) {
+    require_once(__DIR__ . '/controllers/connection.php');
+    if ($_SESSION['is_admin'] !== true) {
         header("Location: login.php");
     }
 
@@ -13,16 +13,98 @@
 
     ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
     
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
         <title>Admin Page</title>
+    <style>
+        header {
+            background-color: #333;
+            display: block;
+            color: #fff;
+            padding: 1%;
+            position:sticky;
+            
+            top:0px;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            display: inline;
+            margin-right: 3%;
+        }
+
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+        #login{
+            display: inline;
+            margin-right : 3%;
+        }
+        /* footer{
+            position:sticky;
+            bottom:0px;
+            background-color: white;
+            color: #fff;
+            padding: 0%;
+        } */
+        .footer{
+            color:black;
+            margin-left:3%;
+            margin-right:3%;
+            margin-top:90%;
+        }
+        hr{
+            color:lightgrey;
+            
+        }
+        .custom-table {
+         width: 100%;
+         border-collapse: collapse;
+         
+        }
+        .custom-table td {
+        border: 1px solid #ccc;
+        padding: 10px;
+        text-align: center;
+        margin-right : 3%;
+         margin-left: 3%;
+        }
+        /* .custom-table th {
+        background-color: #333;
+        color: #fff;
+        } */
+        .custom-table tbody tr:nth-child(4){
+        background-color: #fff;
+        margin-right : 3%;
+        margin-left: 3%;
+        }
+        .delete{
+            background-color: black;
+            display: inline;
+        }
+    </style>
     </head>
 
     <body>
+        <header>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="adminuser.php">Users</a></li>
+                <li><a href="admin.php">Reports</a></li>
+                <div id='login'><a href='controllers/logoutc.php'>Logout</a></div>
+            <!-- <li><a href="#">Services</a></li>-->
+            </ul>
+        </header>
         <h1>Welcome, Administrator!</h1>
         <form action="admin.php" method="post">
             <h3>Add Report</h3>
@@ -54,7 +136,7 @@
             echo "Type: " . $row["report_type"] . "<br>";
             echo "Description: " . $row["description"] . "<br>";
             echo "Time: " . $row["send_time"] . "<br>";
-            echo "<a href='admin.php?delete_report_id=" . $row["report_id"] . "'>Delete</a><br><br>";
+            echo "<div class = 'delete'><a href='admin.php?delete_report_id=" . $row["report_id"] . "'>Delete</a><br><br></div>";
         }
 
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
--- adminuser.php
+++ adminuser.php
@@ -1,20 +1,106 @@
 <?php
-    require_once(__DIR__ .'/controllers/connection.php');
     session_start();
-    if($_SESSION['is_login']!==true){
+    require_once(__DIR__ . '/controllers/connection.php');
+    if($_SESSION['is_admin'] !== true){
         header("Location: login.php");
     }
     ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Admin User</title>
+    <style>
+        header {
+            background-color: #333;
+            display: block;
+            color: #fff;
+            padding: 1%;
+            position:sticky;
+            
+            top:0px;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            display: inline;
+            margin-right: 3%;
+        }
+
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+        #login{
+            display: inline;
+            margin-right : 3%;
+        }
+        /* footer{
+            position:sticky;
+            bottom:0px;
+            background-color: white;
+            color: #fff;
+            padding: 0%;
+        } */
+        .footer{
+            color:black;
+            margin-left:3%;
+            margin-right:3%;
+            margin-top:90%;
+        }
+        hr{
+            color:lightgrey;
+            
+        }
+        .custom-table {
+         width: 100%;
+         border-collapse: collapse;
+         
+        }
+        .custom-table td {
+        border: 1px solid #ccc;
+        padding: 10px;
+        text-align: center;
+        margin-right : 3%;
+         margin-left: 3%;
+        }
+        /* .custom-table th {
+        background-color: #333;
+        color: #fff;
+        } */
+        .custom-table tbody tr:nth-child(4){
+        background-color: #fff;
+        margin-right : 3%;
+        margin-left: 3%;
+        }
+        .delete{
+            background-color: black;
+            display: inline;
+        }
+        .adminn{
+            margin-bottom: 10px;
+        }
+    </style>
     </head>
     
     <body>
+    <header>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="adminuser.php">Users</a></li>
+                <li><a href="admin.php">Reports</a></li>
+                <div id='login'><a href='controllers/logoutc.php'>Logout</a></div>
+            <!-- <li><a href="#">Services</a></li>-->
+            </ul>
+        </header>
+
         <h1>Admin User</h1>
         <form action="adminuser.php" method=post></form>
         <?php
@@ -28,11 +114,14 @@
                 echo 'User ID: ' . $row["user_id"] . '<br>';
                 echo "Name: " . $row["username"] . "<br>";
                 echo "Email: " . $row["email"] . "<br>";
-                echo "<a href='adminuser.php?delete_user_id=" . $row["user_id"] . "'>Delete</a><br><br>";
+                if($row["username"] === 'admin'){
+                    echo "<div class = 'adminn'>Notes: This Account!</div>";
+                    continue;
+                }
+                echo "<div class = 'delete'><a href='admin.php?delete_report_id=" . $row["user_id"] . "'>Delete</a><br><br></div>";
             }
 
-
-            if (isset($_GET['delete_user_id'])) {
+            if (isset($_GET['delete_user_id']) && $_GET['delete_user_id'] !== 'admin') {
                 $user_id = $_GET['delete_user_id'];
                 $delete_query = "DELETE FROM users WHERE user_id = ?";
                 $stmt = $connection->prepare($delete_query);
