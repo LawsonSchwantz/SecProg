8472123ffd803efb8acf95281ead48f6489df702
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2023-12-13 21:46:40 +0700

    Add files via upload

--- controllers/logoutc.php
+++ controllers/logoutc.php
@@ -1,22 +1,9 @@
 <?php
     session_start();
 
-    if($_SESSION['is_admin'] === true){
-        unset($_SESSION['is_admin']);
-        unset($_SESSION['is_login']);
-        unset($_SESSION["user_id"]);
-        unset($_SESSION["name"]);
-        unset($_SESSION["username"]);
-        unset($_SESSION["email"]);
-        unset($_SESSION["phone_number"]);
-        header("Location: ../index.php");
-    }else if($_SESSION['is_login'] === true){
-        unset($_SESSION['is_login']);
-        unset($_SESSION["user_id"]);
-        unset($_SESSION["name"]);
-        unset($_SESSION["username"]);
-        unset($_SESSION["email"]);
-        unset($_SESSION["phone_number"]);
+    if($_SESSION['is_admin'] === true || $_SESSION['is_login'] === true){
+        session_unset();
+        session_destroy();
         header("Location: ../index.php");
     }else{
         header("Location: ../index.php");
